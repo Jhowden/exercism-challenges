6fc78afa75f5d8340d74cf763c530d9c7c5cf442
Jonathan Howden <jonathanhowden@gmail.com>
2015-07-25 13:18:44 -0500

    Refactored out normalizing of string and cleaned up unnecessary code

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,10 +1,9 @@
 class Bob
-  QUESTION = /\?$/
-  END_PUNCTUATION = /(\.|\?|!)$/
-  EMPTY_STRING = ""
+  QUESTION_MARK = "?"
+  UPPERCASE_LETTERS = /[A-Z]/
 
 	def hey( phrase )
-    normalized = normalize_phrase( phrase )
+    normalized = remove_spaces( phrase )
 
     if silence? normalized
       "Fine. Be that way!"
@@ -23,53 +22,15 @@ class Bob
     phrase.empty?
   end
 
-  def answering_question? phrase
-    phrase.match( QUESTION )
+  def answering_question?( phrase )
+    phrase.end_with?( QUESTION_MARK )
   end
 
-  def defuse_yelling? phrase
-    updated_phrase = remove_digits_and_end_punctuation phrase
-    updated_phrase == updated_phrase.upcase && !updated_phrase.empty?
-  end
-
-  def normalize_phrase( phrase )
-    phrase = remove_spaces( phrase )
-    phrase = remove_commas( phrase )
-    phrase = remove_dashes( phrase )
-    phrase = remove_newlines( phrase )
-    phrase
-  end
-
-  def remove_digits_and_end_punctuation( phrase )
-    phrase = remove_digits phrase
-    phrase = remove_end_punctuation phrase
+  def defuse_yelling?( phrase   )
+    phrase == phrase.upcase && phrase.match( UPPERCASE_LETTERS )
   end
 
   def remove_spaces( phrase )
-    phrase.gsub( " ", EMPTY_STRING )
-  end
-
-  def remove_commas( phrase )
-    phrase.gsub( ",", EMPTY_STRING )
-  end
-
-  def remove_dashes( phrase )
-    phrase.gsub( "-", EMPTY_STRING )
-  end
-
-  def remove_apostrophes( phrase )
-    phrase.gsub( "'", EMPTY_STRING )
-  end
-
-  def remove_newlines( phrase )
-    phrase.gsub( "\n", EMPTY_STRING )
-  end
-
-  def remove_digits( phrase )
-    phrase.gsub( /\d/, "" )
-  end
-
-  def remove_end_punctuation( phrase )
-    phrase.gsub( END_PUNCTUATION, "" )
+    phrase.strip
   end
 end
